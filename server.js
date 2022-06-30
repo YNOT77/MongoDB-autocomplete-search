fd0f79bb24718ed9808bc171177cec98329d88d5
Anthony Moran <anthony771998@gmail.com>
2022-06-30 17:58:39 -0400

    Corrected error of data no appending on main.js

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const {MongoClient, ObjectId} = require('mongodb')
-const { urlencoded } = require('body-parser')
 require('dotenv').config()
 const PORT = 8000
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
-    dbName = 'sample_mFlix',
+    dbName = 'sample_mflix',
     collection
 
 
@@ -21,34 +20,34 @@ MongoClient.connect(dbConnectionStr)
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-app.use(cors)
+app.use(cors())
 
-app.get('/search', async(req,res)=>{
-    try{
+app.get("/search", async (request,response) => {
+    try {
         let result = await collection.aggregate([
             {
-                "$Search" : {
-                    "autocomplete": {
-                        "query" : `${req.query.query}`,
+                "$search" : {
+                    "autocomplete" : {
+                        "query": `${request.query.query}`,
                         "path": "title",
-                        "fuzzy":{
-                            "maxEdits": 2,
-                            "prefixLength" : 3
+                        "fuzzy": {
+                            "maxEdits":2,
+                            "prefixLength": 3
                         }
                     }
                 }
             }
         ]).toArray()
-        res.send(result)
-    } catch(error){
-        res.status(500).send({message: error.message})
+        response.send(result)
+    } catch (error) {
+        response.status(500).send({message: error.message})
     }
 })
 
 app.get('/get/:id', async (req, res)=>{
     try{
         let result = await collection.findOne({
-            "_id": ObjectId(request.params.id)
+            "_id": ObjectId(req.params.id)
         })
         res.send(result)
     }catch(error){
